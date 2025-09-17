7045660db88816f5d4c999cece6fe6dbb1970910
priyam <61826890+priyamtrivedi9199@users.noreply.github.com>
2025-09-17 19:22:44 +0530

    Update OtelierBackendApplication.java

--- java/com/otelier/backend/OtelierBackendApplication.java
+++ java/com/otelier/backend/OtelierBackendApplication.java
@@ -1,1 +1,13 @@
+package com.otelier.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@SpringBootApplication
+@EnableMongoAuditing
+public class OtelierBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OtelierBackendApplication.class, args);
+    }
+}
